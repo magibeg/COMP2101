3faac63fdf693f875e03a591aac133f5b9ebfa0d
Ian Dublack <ian.dublack@gmail.com>
2019-01-29 12:46:57 -0800

    updated dice, getpics, improveddice

--- bash/getpics.sh
+++ bash/getpics.sh
@@ -20,7 +20,9 @@ test -f ~/Pictures/pics.zip && unzip -d ~/Pictures -o -q ~/Pictures/pics.zip && 
 # download a tar file from zonzorp
 test -f ~/Pictures/pics.tgz || wget -q -O ~/Pictures/pics.tgz https://zonzorp.net/pics.tgz
 
+#
 test -f ~/Pictures/pics.tgz && tar -xf ~/Pictures/pics.tgz -C ~/Pictures && rm ~/Pictures/pics.tgz
+
 
 # Make a report on what we have in the Pictures directory
 test -d ~/Pictures && cat <<EOF
