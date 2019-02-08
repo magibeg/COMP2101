a3ab138bb75c9ef9ed10b89199c19c2c0d2f4398
Ian Dublack <ian.dublack@gmail.com>
2019-02-08 14:44:23 -0500

    updated numtest

--- bash/Lab3/numtests.sh
+++ bash/Lab3/numtests.sh
@@ -5,13 +5,14 @@
 # TASK 2: Improve it by adding a test to tell the user if the numbers are even or odd
 # TASK 3: Improve it by adding a test to tell the user is the second number is a multiple of the first number
 
-firstNumber=4
-secondNumber=7
+read -p "Give me two numbers separated by a space " firstNumber secondNumber
 
+[ $(($firstNumber%2)) -eq 0 ] && echo "$firstNumber is an even number" || echo "$firstNumber is an odd number"
+[ $(($secondNumber%2)) -eq 0 ] && echo "$secondNumber is an even number" || echo "$secondNumber is an odd number"
 [ $firstNumber -eq $secondNumber ] && echo "The two numbers are the same"
 [ $firstNumber -ne $secondNumber ] && echo "The two numbers are not the same"
 [ $firstNumber -lt $secondNumber ] && echo "The first number is less than the second number"
 [ $firstNumber -gt $secondNumber ] && echo "The first number is greater than the second number"
-
 [ $firstNumber -le $secondNumber ] && echo "The first number is less than or equal to the second number"
 [ $firstNumber -ge $secondNumber ] && echo "The first number is greater than or equal to the second number"
+[ $(($firstNumber%$secondNumber)) -eq 0 ] && echo "The second number is a multiple of the first number" || echo "The second number is not a multiple of the first number"
