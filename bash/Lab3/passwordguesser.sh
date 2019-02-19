b43be0efa6dc4a9161d391ec2f59057a52fe2421
Ian Dublack <ian.dublack@gmail.com>
2019-02-19 11:22:36 -0500

    password guesser added

---
+++ bash/Lab3/passwordguesser.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# This script demonstrates testing to see if 2 strings are the same
+
+# TASK 1: Improve it by asking the user for a password guess instead of using inline literal data
+# TASK 2: Improve it by rewriting it to use the if command
+# TASK 3: Improve it by giving them 3 tries to get it right before failing (test 3 times but only if necessary)
+#           *** Do not use the exit command
+
+referenceString="password"
+maxGuess=2
+counter=0
+
+while [ $counter -le $maxGuess ]
+do
+  echo $counter
+  ((counter++))
+  read -p "What do you think the password is? " myString
+  if [ $myString != $referenceString ]; then
+    echo "You didn't guess the password"
+  else
+    echo "You guessed the password"
+    counter=$(($maxGuess + 1))
+  fi
+done
+#[ $myString = $referenceString ] && echo "You guessed the password!" || echo "The password eludes you..."
