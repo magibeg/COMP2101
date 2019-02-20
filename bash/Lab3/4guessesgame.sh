f52de95b137acb223b6bc035cf7a1f401d7ceb0c
Ian Dublack <ian.dublack@gmail.com>
2019-02-19 19:05:18 -0500

    added cmdargs

--- bash/Lab3/4guessesgame.sh
+++ bash/Lab3/4guessesgame.sh
@@ -17,18 +17,24 @@ EOF
 # Pick the secret number and set their default guess to be a wrong answer
 secretnumber=$(($RANDOM % 10 +1)) # save our secret number to compare later
 userguess=0
-
+counter=0
+maxGuesses=4
 # This loop repeatedly asks the user to guess and tells them if they got the right answer
 # TASK 1: Test the user input to make sure it is not blank
 # TASK 2: Test the user input to make sure it is a number from 1 to 10 inclusive
 # TASK 3: Give them better feedback, telling them if their guess is too low, or too high
 
-while [ $userguess != $secretnumber ]; do # loop around until they get it right
-
+for counter in {1..4}; do
   read -p "Give me a number from 1 to 10: " userguess # ask for a guess
   while [ -z "$userguess" ]; do #&& [ "$userguess" -eq "$userguess" ]; then
     echo "Your guess was empty"
+    ((counter++))
+    if [ $counter -lt $maxGuesses ]; then
     read -p "Give me a number from 1 to 10: " userguess # ask for a guess
+  else
+    echo "Out of guesses"
+    exit
+  fi
   done
   if [ "$userguess" -eq "$userguess" ]; then
     if [ $userguess -eq $secretnumber ]; then
